2b0b6691519f42beb3c3cc3d023a6625107e74ce
Warren Parad <wparad@gmail.com>
2016-11-13 16:24:52 -0500

    Validate ses version.

--- src/ses-forwarder.js
+++ src/ses-forwarder.js
@@ -7,7 +7,10 @@ var forwardTo = 'wparad@gmail.com';
 var bucket = 'email.warrenparad.net';
 
 exports.handler = function(event) {
-	var msgInfo = JSON.parse(event.Records[0].ses);
+	if(event.Records[0].eventSource !== 'aws:ses'  || event.Records[0].eventVersion !== '1.0') {
+		return Promise.resolve('Message is not of the correct versioning "aws:ses" (1.0).')
+	}
+	var msgInfo = event.Records[0].ses;
 	// don't process spam messages
 	if (msgInfo.receipt.spamVerdict.status === 'FAIL' || msgInfo.receipt.virusVerdict.status === 'FAIL') {
 		/*
@@ -25,14 +28,14 @@ exports.handler = function(event) {
 	headers += "Reply-To: " + msgInfo.mail.commonHeaders.from[0] + "\r\n";
 	headers += "X-Original-To: " + msgInfo.mail.commonHeaders.to[0] + "\r\n";
 	headers += "To: " + forwardTo + "\r\n";
-	headers += "Subject: Fwd: " + msgInfo.mail.commonHeaders.subject + "(" +  + ")" + "\r\n";
+	headers += "Subject: SES-Fwd: " + msgInfo.mail.commonHeaders.subject + " (" + msgInfo.mail.messageId.substring(0, 6) + ")" + "\r\n";
 
 	var headerDictionary = {};
 	msgInfo.mail.headers.map(pair => headerDictionary[pair.name] = pair.value );
 
-	if(headerDictionary['Content-Type']) { headers += 'Content-Type: ' + contentType.value; }
-	if(headerDictionary['Content-Transfer-Encoding']) { headers += 'Content-Transfer-Encoding: ' + contentType.value; }
-	if(headerDictionary['MIME-Version']) { headers += 'MIME-Version: ' + contentType.value; }
+	if(headerDictionary['Content-Type']) { headers += 'Content-Type: ' + headerDictionary['Content-Type'] + '\r\n'; }
+	if(headerDictionary['Content-Transfer-Encoding']) { headers += 'Content-Transfer-Encoding: ' + headerDictionary['Content-Transfer-Encoding'] + '\r\n'; }
+	if(headerDictionary['MIME-Version']) { headers += 'MIME-Version: ' + headerDictionary['MIME-Version'] + '\r\n'; }
 
 	return new AWS.S3().getObject({
 		Bucket: bucket,
@@ -43,12 +46,11 @@ exports.handler = function(event) {
 		if (email) {
 			var splitEmail = email.split("\r\n\r\n");
 			splitEmail.shift();
-			email = headers + "\r\n" + splitEmail.join("\r\n\r\n");
+			return headers + "\r\n" + splitEmail.join("\r\n\r\n");
 		}
 		else {
-			email = headers + "\r\n" + "Empty email";
+			return headers + "\r\n" + "Empty email";
 		}
-		return email;
 	})
 	.then(email => {
 		return new AWS.SES().sendRawEmail({ RawMessage: { Data: email } }).promise();
