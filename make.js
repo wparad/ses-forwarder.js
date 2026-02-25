7ae9dd1375dfe33674e3f112cf30947917b4cb5c
Warren Parad <gpg@warrenparad.net>
2026-02-25 11:16:53 +0100

    Convert references from gitlab to github.

--- make.js
+++ make.js
@@ -6,12 +6,32 @@ let path = require('path');
 const aws = require('aws-sdk');
 const commander = require('commander');
 
-let AwsArchitect = require('aws-architect');
-const version = `0.0.${process.env.CI_PIPELINE_ID || '0'}`;
+// const githubActionsRunner = require('ci-build-tools')(process.env.GITHUB_TOKEN);
+function getVersion() {
+  let release_version = '0.0';
+  const pull_request = '';
+  const branch = process.env.GITHUB_REF;
+  const build_number = process.env.GITHUB_RUN_NUMBER;
+
+  //Builds of pull requests
+  if (pull_request && !pull_request.match(/false/i)) {
+    release_version = `0.${pull_request}`;
+  } else if (!branch || !branch.match(/^(refs\/heads\/)?release[/-]/i)) {
+    //Builds of branches that aren't master or release
+    release_version = '0.0';
+  } else {
+    //Builds of release branches (or locally or on server)
+    release_version = branch.match(/^(?:refs\/heads\/)?release[/-](\d+(?:\.\d+){0,3})$/i)[1];
+  }
+  return `${release_version}.${(build_number || '0')}.0.0.0.0`.split('.').slice(0, 3).join('.');
+}
+const version = getVersion();
 commander.version(version);
 
-let packageMetadataFile = path.join(__dirname, 'package.json');
-let packageMetadata = require(packageMetadataFile);
+let AwsArchitect = require('aws-architect');
+
+const packageMetadataFile = path.join(__dirname, 'package.json');
+const packageMetadata = require(packageMetadataFile);
 packageMetadata.version = version;
 
 const REGION = 'us-east-1';
@@ -41,14 +61,14 @@ commander
   .description('Deploy to AWS.')
   .action(async () => {
     // /****** */
-    // process.env.CI_COMMIT_REF_SLUG = 'master';
+    // process.env.GITHUB_REF = 'refs/heads/master';
     // /****** */
     await fs.writeJson(packageMetadataFile, packageMetadata, { spaces: 2 });
 
     const awsArchitect = new AwsArchitect(packageMetadata, apiOptions);
     const stackTemplateProvider = require('./cloudFormationServerlessTemplate');
     const stackTemplate = stackTemplateProvider.getStack();
-    const isMasterBranch = process.env.CI_COMMIT_REF_SLUG === 'master';
+    const isMasterBranch = process.env.GITHUB_REF?.match(/master/);
 
     if (!process.env.WARRENS_PERSONAL_EMAIL || !process.env.WARRENS_EMAIL_DOMAIN) {
       throw Error('WARRENS_PERSONAL_EMAIL environment variable is not set.');
@@ -59,7 +79,7 @@ commander
       await awsArchitect.publishLambdaArtifactPromise();
       if (isMasterBranch) {
         const stackConfiguration = {
-          changeSetName: `${process.env.CI_COMMIT_REF_SLUG}-${process.env.CI_PIPELINE_ID || '1'}-${process.env.CI_JOB_ID || '1'}`,
+          changeSetName: version,
           stackName: packageMetadata.name,
           automaticallyProtectStack: true
         };
@@ -73,7 +93,7 @@ commander
       }
 
       const publicResult = await awsArchitect.publishAndDeployStagePromise({
-        stage: isMasterBranch ? 'production' : process.env.CI_COMMIT_REF_SLUG,
+        stage: isMasterBranch ? 'production' : process.env.GITHUB_REF,
         functionName: packageMetadata.name,
         deploymentKeyName: `${packageMetadata.name}/${version}/lambda.zip`
       });
